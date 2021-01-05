07727ff0bc3435ab5152c10b6a4ad966360b25e0
Sagar Rawat <70856578+Rawat-Sagar@users.noreply.github.com>
2021-01-05 23:20:17 +0530

    Add files via upload

---
+++ js_operators.js
@@ -0,0 +1,51 @@
+// Operators in Javascript
+
+let a = 34;
+let b = 56;
+console.log("The value of a+b is ", a+b);
+console.log("The value of a-b is ", a-b);
+console.log("The value of a*b is ", a*b);
+console.log("The value of a/b is ", a/b);
+
+
+// Assignment Opeartors
+let c = b;
+console.log("c  : ",c);
+c+=2;
+c-=2;
+c*=2;
+c/=2;
+console.log("The value of c+=2 : ",c+=2);
+console.log("The value of c-=2 : ",c-=2);
+console.log("The value of c*=2 : ",c*=2);
+console.log("The value of c/=2 : ",c/=2);
+
+// Comparison Operator
+let x=20 , y=30;
+console.log('x : ',x,'y : ',y)
+console.log("x==y : ",x==y);
+console.log("x>=y : ",x>=y);
+console.log("x<=y : ",x<=y);
+console.log("x!=y : ",x!=y);
+
+// logical operators
+
+
+// Logical AND
+console.log("true && true :",true && true);
+console.log("true && false :",true && false);
+console.log("false && false :",false && false);
+console.log( "false && true :",false && true);
+
+
+//Logical OR
+console.log("true || false :",true || false);
+console.log("false || true :",false || true);
+console.log("true || true :",true || true);
+console.log("false || false :",false || false);
+
+//Logical NOT
+console.log("!false :" ,!false);
+console.log("!true :" ,!true);
+
+
