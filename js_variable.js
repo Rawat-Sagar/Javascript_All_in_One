b07a597372db542f2b4eb013856c10779c1cda8e
Sagar Rawat <70856578+Rawat-Sagar@users.noreply.github.com>
2021-01-05 23:20:45 +0530

    Add files via upload

---
+++ js_variable.js
@@ -0,0 +1,59 @@
+// Javascript Variable
+// containers to store data values.
+let num = 34;
+let num1 = 56;
+console.log(num+num1);
+
+
+// Data types in JS
+
+// Numbers
+let num2 = 455;
+
+// String
+let str1 = "This is a string";
+
+// Objects
+let marks = {
+    sagar : 60,
+    sanjay : 58,
+    harry : 48
+}
+
+console.log(num2);
+console.log(marks);
+console.log(str1);
+
+
+//Booleans 
+let a = true;
+let b = false;
+console.log(a,b);
+
+
+
+// Undefined or null
+let und = undefined;
+console.log("und :",und);
+console.log(undefined);
+
+let und1;
+console.log(und1);
+
+let n = null;
+console.log(n);
+
+/*
+ 2 types of datatypes in JS.
+ a.Primitive datatypes.:undefined , null , number , string , boolean ,symbol
+ b.Reference datatypes.:Arrays , Objects.
+
+*/
+
+// Arrays
+let arr = [1,2,3,4,"sagar"]
+console.log(arr);
+console.log(arr[4]);
+
+
+
