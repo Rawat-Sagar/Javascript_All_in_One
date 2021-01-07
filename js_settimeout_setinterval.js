3b9d2d4bf2707b38cb2b7dd83dd305e53b79a2cb
Sagar Rawat <70856578+Rawat-Sagar@users.noreply.github.com>
2021-01-07 23:17:44 +0530

    Add files via upload

---
+++ js_settimeout_setinterval.js
@@ -0,0 +1,31 @@
+// Arrow Functions
+sum = (a,b)=>{
+    return a+b;
+}
+console.log(sum(10,20));
+
+
+logkaro = ()=>{
+    console.log("I am ur log");
+}
+a = setTimeout(logkaro , 5000);
+// if u want to clear the timeout then unci=omment it.
+console.log(clearTimeout(a))
+
+clr = setInterval(logkaro,2000);
+// if u want to clear the interval then uncomment it.
+console.log(clearInterval(clr));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
